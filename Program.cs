475b81f841d2fc2be9c8a2b441dda4afba7b89b7
zenin5479 <zenin5479@gmail.com>
2024-09-26 22:48:09 +0300

    Update 0.2 Обновлен метод

--- Program.cs
+++ Program.cs
@@ -5,33 +5,29 @@ namespace CodeExperiment
 {
    internal class Program
    {
+      public const double Pi = 3.1415926535897931;
+      public const double E = 2.7182818284590451;
+
       static void Main()
       {
-         double[] x = { 100, 150, 170, 91, 30, 67, 79, 87, 96, 200 };
-         double s = x.Average();
-
+         //double pi = ?;
+         
          double fi = (1 + Math.Sqrt(5)) / 2;
          Console.WriteLine(fi);
+         double exp = Math.Exp(1);
+         Console.WriteLine(exp);
+
+
+         double[] x = { 100, 150, 170, 91, 30, 67, 79, 87, 96, 200 };
+         double s = x.Average();
          for (int i = 0; i < x.Length; i++)
          {
             double y = Math.Exp(x[i] / 6);
-            double exp = Math.Exp(1);
-            Console.WriteLine(exp);
+
             Console.WriteLine(y);
          }
 
-
-
          Console.WriteLine(s);
-
-
-
-
-
-
-
       }
-
-
    }
 }
