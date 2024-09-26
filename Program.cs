614892a4ff6f98b9ed37f3045d67a9a9bdf17572
zenin5479 <zenin5479@gmail.com>
2024-09-26 22:16:37 +0300

    Update 0.1 Обновлен метод

--- Program.cs
+++ Program.cs
@@ -7,23 +7,31 @@ namespace CodeExperiment
    {
       static void Main()
       {
-         double[] x = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-         int n = x.Length;
+         double[] x = { 100, 150, 170, 91, 30, 67, 79, 87, 96, 200 };
          double s = x.Average();
-         double y = Ema(s, n);
-         Console.WriteLine(y);
+
+         double fi = (1 + Math.Sqrt(5)) / 2;
+         Console.WriteLine(fi);
+         for (int i = 0; i < x.Length; i++)
+         {
+            double y = Math.Exp(x[i] / 6);
+            double exp = Math.Exp(1);
+            Console.WriteLine(exp);
+            Console.WriteLine(y);
+         }
+
+
+
+         Console.WriteLine(s);
+
+
+
+
+
+
+
       }
 
-      public static double Ema(double s, int n)
-      {
-         // x - это входная серия            
-         // N - условный возраст используемых данных
-         // k - константа сглаживания
 
-         double k = 2.0 / (n + 1);
-         double y = k * s + (1 - k) * s;
-
-         return y;
-      }
    }
 }
