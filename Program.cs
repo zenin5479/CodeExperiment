fdb31a9ac6bcd0c601532e45015ac4dacd9059ab
zenin5479 <zenin5479@gmail.com>
2024-09-27 20:58:57 +0300

    Update 0.4 Обновлен метод

--- Program.cs
+++ Program.cs
@@ -5,31 +5,36 @@ namespace CodeExperiment
 {
    internal class Program
    {
+      public const double Tau = 6.2831853071795862;
       public const double Pi = 3.1415926535897931;
       public const double E = 2.7182818284590451;
+      public const double Fi = 1.618033988749895;
 
       static void Main()
       {
-         //double pi = ?;
-
+         Console.WriteLine(Tau);
          Console.WriteLine(Pi);
          Console.WriteLine(E);
-         double fi = (1 + Math.Sqrt(5)) / 2;
-         Console.WriteLine(fi);
-         double exp = Math.Exp(1);
-         Console.WriteLine(exp);
+         Console.WriteLine(Fi);
 
+         //double fi = (1 + Math.Sqrt(5)) / 2;
+         //Console.WriteLine(fi);
+         //double exp = Math.Exp(1);
+         //Console.WriteLine(exp);
 
          double[] x = { 100, 150, 170, 91, 30, 67, 79, 87, 96, 200 };
          double s = x.Average();
-         for (int i = 0; i < x.Length; i++)
-         {
-            double y = Math.Exp(x[i] / 6);
-
-            Console.WriteLine(y);
-         }
-
          Console.WriteLine(s);
+         double tau = s / Tau;
+         Console.WriteLine(tau);
+         double pi = s / Pi;
+         Console.WriteLine(pi);
+         double e = s / E;
+         Console.WriteLine(e);
+         double fi = s / Fi;
+         Console.WriteLine(fi);
+
+         Console.ReadKey();
       }
    }
 }
