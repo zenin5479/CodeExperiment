71b11b10354b39b99fd1554cdb9ae5a29a110cb0
zenin5479 <zenin5479@gmail.com>
2024-09-28 22:35:09 +0300

    Update 1.1 Обновлен метод

--- Program.cs
+++ Program.cs
@@ -5,43 +5,22 @@ namespace CodeExperiment
 {
    internal class Program
    {
-      //public const double Tau = 6.283185307179586;
-      //public const double Pi = 3.141592653589793;
-      //public const double E = 2.718281828459045;
       // Божественная пропорция
       public const double Fi = 1.618033988749895;
       public const double Fihalf = 0.809016994374947;
-      // Постоянная Гаусса
-      //public const double G = 0.834626841674073;
 
       static void Main()
       {
-         //Console.WriteLine(Tau);
-         //Console.WriteLine(Pi);
-         //Console.WriteLine(E);
          Console.WriteLine(Fi);
-         //Console.WriteLine(Fihalf);
-         //Console.WriteLine(G);
-
-         //double fi = (1 + Math.Sqrt(5)) / 2;
-         //Console.WriteLine(fi);
-         //double exp = Math.Exp(1);
-         //Console.WriteLine(exp);
+         Console.WriteLine(Fihalf);
 
          int data = 300;
          double[] x = { 100, 150, 170, 250, 30, 280, 190, 87, 96, 200 };
          double s = x.Average();
          Console.WriteLine(s);
 
-         //double tau1 = Convert.ToInt32(s / Tau);
-         //Console.WriteLine(tau1);
-         //double pi1 = Convert.ToInt32(s / Pi);
-         //Console.WriteLine(pi1);
-         //double e1 = Convert.ToInt32(s / E);
-         //Console.WriteLine(e1);
-         double fidata = Convert.ToInt32(data * Fihalf);
-         Console.WriteLine(fidata);
-
+         double fihalfdata = data * Fihalf;
+         Console.WriteLine(fihalfdata);
 
          double fi1 = Convert.ToInt32(s / Fi);
          Console.WriteLine(fi1);
