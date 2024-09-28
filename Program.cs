d591a8c23bbb55bb15227e3b37b61630652c84e4
zenin5479 <zenin5479@gmail.com>
2024-09-28 22:32:10 +0300

    Update 1.0 Обновлен метод

--- Program.cs
+++ Program.cs
@@ -5,22 +5,23 @@ namespace CodeExperiment
 {
    internal class Program
    {
-      public const double Tau = 6.283185307179586;
-      public const double Pi = 3.141592653589793;
-      public const double E = 2.718281828459045;
+      //public const double Tau = 6.283185307179586;
+      //public const double Pi = 3.141592653589793;
+      //public const double E = 2.718281828459045;
       // Божественная пропорция
       public const double Fi = 1.618033988749895;
+      public const double Fihalf = 0.809016994374947;
       // Постоянная Гаусса
-      public const double G = 0.834626841674073;
+      //public const double G = 0.834626841674073;
 
       static void Main()
       {
-         Console.WriteLine(Tau);
-         Console.WriteLine(Pi);
-         Console.WriteLine(E);
+         //Console.WriteLine(Tau);
+         //Console.WriteLine(Pi);
+         //Console.WriteLine(E);
          Console.WriteLine(Fi);
-         Console.WriteLine(Fi / 2);
-         Console.WriteLine(G);
+         //Console.WriteLine(Fihalf);
+         //Console.WriteLine(G);
 
          //double fi = (1 + Math.Sqrt(5)) / 2;
          //Console.WriteLine(fi);
@@ -32,12 +33,16 @@ namespace CodeExperiment
          double s = x.Average();
          Console.WriteLine(s);
 
-         double tau1 = Convert.ToInt32(s / Tau);
-         Console.WriteLine(tau1);
-         double pi1 = Convert.ToInt32(s / Pi);
-         Console.WriteLine(pi1);
-         double e1 = Convert.ToInt32(s / E);
-         Console.WriteLine(e1);
+         //double tau1 = Convert.ToInt32(s / Tau);
+         //Console.WriteLine(tau1);
+         //double pi1 = Convert.ToInt32(s / Pi);
+         //Console.WriteLine(pi1);
+         //double e1 = Convert.ToInt32(s / E);
+         //Console.WriteLine(e1);
+         double fidata = Convert.ToInt32(data * Fihalf);
+         Console.WriteLine(fidata);
+
+
          double fi1 = Convert.ToInt32(s / Fi);
          Console.WriteLine(fi1);
          double result = Convert.ToInt32(data / (Fi * 10));
