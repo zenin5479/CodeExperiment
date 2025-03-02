514130a648abbc711c313d62a46d3640bd1efce0
zenin5479 <zenin5479@gmail.com>
2025-03-02 16:37:34 +0300

    Update 2.0 Обновлен метод Конвертация строкового одномерного массива в числовой без функции

--- Program.cs
+++ Program.cs
@@ -8,15 +8,9 @@ namespace CodeExperiment
    {
       static void Main()
       {
-         double MyParse(string x)
-         {
-            double y = double.Parse(x);
-            return y;
-         }
-
          string str = "6,0 5,0 9,3 7,8 5,7 8,1 1,2";
          string[] s = str.Split();
-         List<double> list = s.Select(MyParse).ToList();
+         List<double> list = s.Select(double.Parse).ToList();
          foreach (double rs in list)
          {
             Console.WriteLine(rs);
