acbcafe4d320e5fc45670a2041011f04b77f3789
zenin5479 <zenin5479@gmail.com>
2025-03-03 20:20:07 +0300

    Update 2.6 Обновлен метод Конвертация строкового двумерного массива в числовой

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace CodeExperiment
 {
@@ -6,23 +7,34 @@ namespace CodeExperiment
    {
       static void Main()
       {
-         string[,] txtNum = { { "21,5", "123,1", "87,8" }, { "54,3", "2,7", "0,8" }, { "0,3", "7,9", "4,5" } };
-         double[,] number = new double[txtNum.GetLength(0), txtNum.GetLength(1)];
-         for (int i = 0; i < txtNum.GetLength(0); i++)
+         //string[,] txtNum = { { "21,5", "123,1", "87,8" }, { "54,3", "2,7", "0,8" }, { "0,3", "7,9", "4,5" } };
+         //double[,] number = new double[txtNum.GetLength(0), txtNum.GetLength(1)];
+         //for (int i = 0; i < txtNum.GetLength(0); i++)
+         //{
+         //   for (int j = 0; j < txtNum.GetLength(1); j++)
+         //   {
+         //      number[i, j] = Convert.ToDouble(txtNum[i, j]);
+         //      Console.Write(number[i, j] + " ");
+         //   }
+
+         //   Console.WriteLine();
+         //}
+
+         //Console.WriteLine("Диагональ массива");
+         //Console.WriteLine(number[0, 0]);
+         //Console.WriteLine(number[1, 1]);
+         //Console.WriteLine(number[2, 2]);
+
+
+         using (StreamReader sr = new StreamReader("a.txt"))
          {
-            for (int j = 0; j < txtNum.GetLength(1); j++)
-            {
-               number[i, j] = Convert.ToDouble(txtNum[i, j]);
-               Console.Write(number[i, j] + " ");
-            }
-
-            Console.WriteLine();
+            string num = sr.ReadLine();
+            if (num != null)
+               foreach (string number in num.Split())
+               {
+                  Console.WriteLine(number);
+               }
          }
-
-         Console.WriteLine("Диагональ массива");
-         Console.WriteLine(number[0, 0]);
-         Console.WriteLine(number[1, 1]);
-         Console.WriteLine(number[2, 2]);
 
          Console.ReadKey();
       }
