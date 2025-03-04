c071378315e29230661e2878bc741a53bc5017af
zenin5479 <zenin5479@gmail.com>
2025-03-04 21:23:29 +0300

    Update 3.1 Обновлен метод Конвертация строкового двумерного массива в числовой

--- Program.cs
+++ Program.cs
@@ -9,26 +9,26 @@ namespace CodeExperiment
    {
       static void Main()
       {
-         //double MyParse(string x)
-         //{
-         //   double y = double.Parse(x);
-         //   return y;
-         //}
-
-         //string str = "6,0 5,0 9,3 7,8 5,7 8,1 1,2";
-         //string[] s = str.Split();
-         //List<double> list = s.Select(MyParse).ToList();
-         //foreach (double rs in list)
-         //{
-         //   Console.WriteLine(rs);
-         //}
+         double MyParse(string x)
+         {
+            double y = double.Parse(x);
+            return y;
+         }
 
          string str = "6,0 5,0 9,3 7,8 5,7 8,1 1,2";
          string[] s = str.Split();
-         List<double> list = s.Select(double.Parse).ToList();
+         List<double> list = s.Select(MyParse).ToList();
          foreach (double rs in list)
          {
             Console.WriteLine(rs);
+         }
+
+         string str2 = "6,0 5,0 9,3 7,8 5,7 8,1 1,2";
+         string[] s2 = str2.Split();
+         List<double> list2 = s2.Select(double.Parse).ToList();
+         foreach (double rx in list2)
+         {
+            Console.WriteLine(rx);
          }
 
          string[,] txtNum = { { "21,5", "123,1", "87,8" }, { "54,3", "2,7", "0,8" }, { "0,3", "7,9", "4,5" } };
