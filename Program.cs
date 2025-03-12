5ff17cbacfb4391d31a3eee782c16004f19ac857
zenin5479 <zenin5479@gmail.com>
2025-03-12 21:50:15 +0300

    Update 8.2 Обновлен метод Конвертация строкового двумерного массива в числовой

--- Program.cs
+++ Program.cs
@@ -9,34 +9,6 @@ namespace CodeExperiment
 
       static void Main()
       {
-         bool isEqual = true;
-         int[] array1 = { 10, 20, 30, 40, 50 };
-         int[] array2 = { 10, 20, 30, 40, 50 };
-         if (array1.Length == array2.Length)
-         {
-            for (int i = 0; i < array1.Length; i++)
-            {
-               if (array1[i] != array2[i])
-               {
-                  isEqual = false;
-                  break;
-               }
-            }
-         }
-         else
-         {
-            isEqual = false;
-         }
-
-         if (isEqual)
-         {
-            Console.WriteLine("Массивы равны");
-         }
-         else
-         {
-            Console.WriteLine("Массивы не равны");
-         }
-
          int n = 10;
          int m = 20;
 
