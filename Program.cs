31928284194333e44ee847e8596c5f3119298887
zenin5479 <zenin5479@gmail.com>
2025-03-25 22:16:19 +0300

    Update 12.3 Обновлен метод. Конвертация строкового двумерного массива в числовой.

--- Program.cs
+++ Program.cs
@@ -15,64 +15,49 @@ namespace CodeExperiment
          string filePath = AppContext.BaseDirectory + "a.txt";
          try
          {
-            using StreamReader sr = new StreamReader(filePath);
-            //Это позволяет вам выполнить одну операцию чтения
-            Console.WriteLine(sr.ReadToEnd());
+            FileStream fpA = File.Open(filePath, FileMode.Open, FileAccess.Read);
+            if (fpA == null)
+            {
+               Console.WriteLine("Ошибка при открытии файла для чтения");
+            }
+            // Cвязываем StreamReader c файловыйм потоком
+            if (fpA != null)
+            {
+               // Классический вариант чтения файла построчно
+               StreamReader readerone = new StreamReader(fpA);
+               string[] arrayone = new string[n];
+               int k = 0;
+               while (!readerone.EndOfStream)
+               {
+                  string stroka = readerone.ReadLine();
+                  arrayone[k] = stroka;
+                  Console.WriteLine(stroka);
+                  k++;
+               }
+               readerone.Close();
+               Console.WriteLine();
+               // Разделение строки на подстроки и конвертация подстрок в double
+               int z = 0;
+               while (z < arrayone.GetLength(0))
+               {
+                  string[] arraysplit = arrayone[z].Split(" ");
+                  int x = 0;
+                  while (x < arraysplit.GetLength(0))
+                  {
+                     arrayForFileSize[z, x] = Convert.ToDouble(arraysplit[x]);
+                     Console.Write(arrayForFileSize[z, x] + " ");
+                     x++;
+                  }
+                  Console.WriteLine();
+                  z++;
+               }
+               Console.WriteLine();
+            }
          }
          catch (Exception e)
          {
             Console.WriteLine("Процесс завершился неудачей: {0}", e);
          }
-
-         // Классический вариант чтения файла построчно
-         StreamReader readerone = new StreamReader(filePath);
-         string[] arrayone = new string[n];
-         int k = 0;
-         while (!readerone.EndOfStream)
-         {
-            string stroka = readerone.ReadLine();
-            arrayone[k] = stroka;
-            Console.WriteLine(stroka);
-            k++;
-         }
-         Console.WriteLine();
-         // Разделение строки на подстроки и конвертация подстрок в double
-         int z = 0;
-         while (z < arrayone.GetLength(0))
-         {
-            string[] arraysplit = arrayone[z].Split(" ");
-            int x = 0;
-            while (x < arraysplit.GetLength(0))
-            {
-               arrayForFileSize[z, x] = Convert.ToDouble(arraysplit[x]);
-               Console.Write(arrayForFileSize[z, x] + " ");
-               x++;
-            }
-            Console.WriteLine();
-            z++;
-         }
-         readerone.Close();
-         Console.WriteLine();
-
-         FileStream fpA = File.Open(filePath, FileMode.Open, FileAccess.Read);
-         if (fpA == null)
-         {
-            Console.WriteLine("Ошибка при открытии файла для чтения");
-         }
-         // Cвязываем StreamReader c файловыйм потоком
-         if (fpA != null)
-         {
-            StreamReader readerfour = new StreamReader(fpA);
-            for (int i = 0; i < n; i++)
-            {
-               // Метод ReadLine() считает одну строку и перенесет указатель на начало следующей строки
-               string singleLine = readerfour.ReadLine();
-               Console.WriteLine(singleLine);
-            }
-            // Закрытие потока
-            readerfour.Close();
-         }
-         Console.WriteLine();
 
          Console.ReadKey();
       }
