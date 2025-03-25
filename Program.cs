59fdfcd5b8937c955d7a92e246e6aa5033a65708
zenin5479 <zenin5479@gmail.com>
2025-03-25 21:42:38 +0300

    Update 12.1 Обновлен метод. Конвертация строкового двумерного массива в числовой.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 
 namespace CodeExperiment
@@ -10,75 +9,81 @@ namespace CodeExperiment
       {
          int n = 10;
          int m = 20;
-         string filePath = AppContext.BaseDirectory + "a.txt";
-         double[,] arrayForFileSize = new double[n, m];
 
-         // Классический вариант чтения файла построчно
+         double[,] arrayForFileSize = new double[n, m];
+         string filePath = AppContext.BaseDirectory + "a.txt";
          try
          {
-            FileStream fpA = File.Open(filePath, FileMode.Open, FileAccess.Read);
-            if (fpA == null)
-            {
-               Console.WriteLine("Ошибка при открытии файла для чтения");
-            }
-            // Cвязываем StreamReader c файловыйм потоком
-            if (fpA != null)
-            {
-               StreamReader readerone = new StreamReader(filePath);
-               while (!readerone.EndOfStream)
-               {
-                  Console.WriteLine(readerone.ReadLine());
-               }
-               // Закрытие потока
-               readerone.Close();
-
-               string[] arrayone = new string[n];
-               Console.WriteLine();
-               // Разделение строки на подстроки и конвертация подстрок в double
-               int c = 0;
-               while (c < arrayone.GetLength(0))
-               {
-                  string[] arraysplit = arrayone[c].Split(" ");
-                  int x = 0;
-                  while (x < arraysplit.GetLength(0))
-                  {
-                     arrayForFileSize[c, x] = Convert.ToDouble(arraysplit[x]);
-                     Console.Write(arrayForFileSize[c, x] + " ");
-                     x++;
-                  }
-                  Console.WriteLine();
-                  c++;
-               }
-               Console.WriteLine();
-            }
-
-            Console.WriteLine();
+            using StreamReader sr = new StreamReader(filePath);
+            //Это позволяет вам выполнить одну операцию чтения
+            Console.WriteLine(sr.ReadToEnd());
          }
          catch (Exception e)
          {
-            Console.WriteLine("Процесс завершился неудачей {0}", e);
+            Console.WriteLine("Процесс завершился неудачей: {0}", e);
          }
 
-         // Для создания списка списков необходимо создать отдельные списки
-         // и использовать List<T>.Add(T) для добавления их в основной список
-         List<List<string>> itemBag = new List<List<string>>();
-         List<string> weapons = new List<string> { "Меч", "Кинжал", "Арбалет" };
-         itemBag.Add(weapons);
-         List<string> potions = new List<string> { "Зелье здоровья", "Зелье силы", "Зелье удачи" };
-         itemBag.Add(potions);
-         int z = 0;
-         Console.WriteLine("Количество листов {0}", itemBag.Count);
-         while (z < itemBag.Count)
+         // Классический вариант чтения файла построчно
+         StreamReader readerone = new StreamReader(filePath);
+         string[] arrayone = new string[n];
+         int k = 0;
+         while (!readerone.EndOfStream)
          {
-            Console.WriteLine("Лист {0}: количество элементов во вложенном листе {1}", z, itemBag[z].Count);
+            string stroka = readerone.ReadLine();
+            arrayone[k] = stroka;
+            Console.WriteLine(stroka);
+            k++;
+         }
+         Console.WriteLine();
+         // Разделение строки на подстроки и конвертация подстрок в double
+         int z = 0;
+         while (z < arrayone.GetLength(0))
+         {
+            string[] arraysplit = arrayone[z].Split(" ");
             int x = 0;
-            while (x < itemBag[z].Count)
+            while (x < arraysplit.GetLength(0))
             {
-               Console.Write("{0} ", itemBag[z][x]);
+               arrayForFileSize[z, x] = Convert.ToDouble(arraysplit[x]);
+               Console.Write(arrayForFileSize[z, x] + " ");
                x++;
             }
             Console.WriteLine();
             z++;
+         }
+         readerone.Close();
+         Console.WriteLine();
+
+         FileStream fpA = File.Open(filePath, FileMode.Open, FileAccess.Read);
+         if (fpA == null)
+         {
+            Console.WriteLine("Ошибка при открытии файла для чтения");
+         }
+         // Cвязываем StreamReader c файловыйм потоком
+         if (fpA != null)
+         {
+            StreamReader readerfour = new StreamReader(fpA);
+            for (int i = 0; i < n; i++)
+            {
+               // Метод ReadLine() считает одну строку и перенесет указатель на начало следующей строки
+               string singleLine = readerfour.ReadLine();
+               Console.WriteLine(singleLine);
+            }
+            // Закрытие потока
+            readerfour.Close();
+         }
+         Console.WriteLine();
+
+         // Конвертируем массив строк
+         string[,] txtNum = { { "21,5", "123,1", "87,8" }, { "54,3", "2,7", "0,8" }, { "0,3", "7,9", "4,5" } };
+         double[,] number = new double[txtNum.GetLength(0), txtNum.GetLength(1)];
+         for (int i = 0; i < txtNum.GetLength(0); i++)
+         {
+            for (int j = 0; j < txtNum.GetLength(1); j++)
+            {
+               number[i, j] = Convert.ToDouble(txtNum[i, j]);
+               Console.Write(number[i, j] + " ");
+            }
+            Console.WriteLine();
          }
          Console.WriteLine();
 
