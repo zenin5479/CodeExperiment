b9b5ebb00d5e2fef9a18058a23bd7774542c8977
zenin5479 <zenin5479@gmail.com>
2025-04-08 22:51:21 +0300

    Update 28.2 Обновлен метод. Конвертация строкового двумерного массива в числовой.

--- Program.cs
+++ Program.cs
@@ -126,6 +126,34 @@ namespace CodeExperiment
                }
                Console.WriteLine("Количество пробелов {0}", whiteSpace.Count);
                //Console.WriteLine();
+
+               //// Метод String.IndexOf(char, int, int)
+               //string str = "1,5 5,6 9,8 2,1 5,8 9,1 7,3 4,2 2,9 1,7";
+               //Console.WriteLine("Метод String.IndexOf(char, int, int) генерирует следующий результат");
+               //Console.WriteLine("{0}", str);
+               //FindAllChar(',', str);
+               //FindAllChar(' ', str);
+               //Console.WriteLine();
+
+               //char target = ' ';
+               //Console.Write("Символ '{0}' встречается в позиции(ах): ", target);
+               //int startIndex = -1;
+               //// Поиск всех позиций где встречается символ в объекте
+               //while (true)
+               //{
+               //   startIndex = s.IndexOf(target, startIndex + 1, s.Length - startIndex - 1);
+               //   // Выход из цикла, если символ не найден
+               //   if (startIndex < 0)
+               //   {
+               //      break;
+               //   }
+
+               //   Console.Write("{0}, ", startIndex);
+               //}
+
+               //Console.WriteLine();
+
+               //Console.WriteLine();
             }
          }
          catch (Exception e)
@@ -135,6 +163,28 @@ namespace CodeExperiment
 
          //Console.WriteLine();
          Console.ReadKey();
+      }
+
+      static void FindAllChar(char target, string searched)
+      {
+         Console.Write("Символ '{0}' встречается в позиции(ях): ", target);
+         int startIndex = -1;
+         int hitCount = 0;
+         // Поиск всех позиций где встречается символ в объекте
+         while (true)
+         {
+            startIndex = searched.IndexOf(target, startIndex + 1, searched.Length - startIndex - 1);
+            // Выход из цикла, если символ не найден
+            if (startIndex < 0)
+            {
+               break;
+            }
+
+            Console.Write("{0}, ", startIndex);
+            hitCount++;
+         }
+
+         Console.WriteLine("всего позиций: {0}", hitCount);
       }
    }
 }
