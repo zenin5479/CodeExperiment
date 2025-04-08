f68a36710e516704cac6bfee0173f366bb17cf15
zenin5479 <zenin5479@gmail.com>
2025-04-08 13:39:17 +0300

    Update 26.4 Обновлен метод. Конвертация строкового двумерного массива в числовой.

--- Program.cs
+++ Program.cs
@@ -162,16 +162,6 @@ namespace CodeExperiment
 
                Console.WriteLine();
 
-
-
-
-
-
-
-
-
-
-
                // Метод String.IndexOf(char, int, int)
                string str = "1,5 5,6 9,8 2,1 5,8 9,1 7,3 4,2 2,9 1,7";
                Console.WriteLine("Метод String.IndexOf(char, int, int) генерирует следующий результат");
