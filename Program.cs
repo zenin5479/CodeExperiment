3b81d96f402b52935a65f93499ec0acdcb6b717a
zenin5479 <zenin5479@gmail.com>
2025-04-10 13:22:27 +0300

    Update 29.9 Обновлен метод. Конвертация строкового двумерного массива в числовой.

--- Program.cs
+++ Program.cs
@@ -116,8 +116,6 @@ namespace CodeExperiment
                string s = "1,5 5,6 9,8 2,1 5,8 9,1 7,3 4,2 2,9 1,7";
                char charRange = ' ';
                List<int> whiteSpace = new List<int>();
-               
-
                for (int i = 0; i < s.Length; i++)
                {
                   if (charRange.Equals(s[i]))
