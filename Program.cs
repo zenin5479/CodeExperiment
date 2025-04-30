48f82a1e4944c75a33bfcec82c3103840284868b
zenin5479 <zenin5479@gmail.com>
2025-04-30 17:03:10 +0300

    Update 54.3 Обновлен метод

--- Program.cs
+++ Program.cs
@@ -199,33 +199,9 @@ namespace CodeExperiment
 
          Console.WriteLine();
 
-         string original= "Процесс завершился неудачей";
-         int linesPerSegment;
-         List<string> segments = new List<string>();
-
-         int startIndex = 0;
-         int newLinesEncountered = 0;
-
-         for (int i = 0; i < original.Length; i++)
-         {
-            if (original[i] == '\n')
-            {
-               newLinesEncountered++;
-            }
-
-            if (newLinesEncountered == linesPerSegment || i == original.Length - 1)
-            {
-               segments.Add(original.Substring(startIndex, (i - startIndex + 1)));
-               startIndex = i + 1;
-               newLinesEncountered = 0;
-            }
-         }
-
-
-
          //FileReadLines(filePath);
          //FileReadAllLines(filePath);
-         //StreamReaderReadLine(filePath);
+         StreamReaderReadLine(filePath);
          //StreamReaderReadToEnd(filePath);
 
          Console.ReadKey();
