14e9ff0d9a91dead857377c3a63ab60cddfaf53c
zenin5479 <zenin5479@gmail.com>
2025-05-02 11:07:40 +0300

    Update 59.6 Обновлен метод

--- Program.cs
+++ Program.cs
@@ -168,15 +168,14 @@ namespace CodeExperiment
             Console.WriteLine();
             // Проверка последнего элемента первой строки
             Console.WriteLine("Последний элемент первой строки : {0}", arrayFour[0, arrayFour.GetLength(1) - 1]);
+            int[] stringArray = { 5, 5, 8, 6 };
+            int index = Array.FindIndex(stringArray, x => x == stringArray[^1]);
+            Console.WriteLine("Индекс последнего элемента первой строки : {0}", arrayFour[0, arrayFour.GetLength(1) - 1]);
          }
 
          Console.WriteLine();
-         int[] stringArray = { 5, 5, 8, 6 };
-         int elementToFind = stringArray[stringArray.Length-1];
 
-         int index = Array.FindIndex(stringArray, x => x == elementToFind);
-         Console.WriteLine(index);
-         
+
 
          FileReadAllLines(filePath);
          Console.ReadKey();
