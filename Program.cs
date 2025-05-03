74e84ff9bf01b9bd0b808d77055098727c0ed900
zenin5479 <zenin5479@gmail.com>
2025-05-03 12:13:21 +0300

    Update 61.5 Обновлен метод

--- Program.cs
+++ Program.cs
@@ -185,7 +185,7 @@ namespace CodeExperiment
                iterator--;
             }
 
-            Console.WriteLine("Элемент {0} строки: {1}; Индекс элемента: {2}  ", bar, variable, index);
+            Console.WriteLine("Элемент {0} строки: {1} \nИндекс: {2}  ", bar, variable, index);
          }
 
          Console.WriteLine();
