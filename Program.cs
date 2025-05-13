90056071a37344c2b572d88442bcdc31c0f604fc
zenin5479 <zenin5479@gmail.com>
2025-05-13 16:35:04 +0300

    Update 69.2 Метод File.WriteAllLines для записи массива строк в файл

--- Program.cs
+++ Program.cs
@@ -240,19 +240,7 @@ namespace CodeExperiment
             }
 
             Console.ResetColor();
-            //Console.WriteLine();
-            // Проверка всех имеющихся элементов в строке
-            int lines = 2;
-            int range = arrayDouble.GetLength(1);
-            int iterator = 0;
-            while (iterator < range)
-            {
-               //Console.WriteLine("Элемент {0} строки по индексу {1} равен: {2}", lines, iterator, arrayDouble[lines, iterator]);
-               iterator++;
-            }
          }
-
-         Console.ReadKey();
       }
    }
 }
