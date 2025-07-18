b62a7ce412746964c8d3cfa60c85033ba3d08b3c
zenin5479 <zenin5479@gmail.com>
2025-07-18 15:02:46 +0300

    Update 84.5 рефакторинг. Выходные параметры. Модификатор out

--- Program.cs
+++ Program.cs
@@ -12,15 +12,24 @@ namespace CodeExperiment
          int i;
          double myDr, mySqr, mySqrt, myD = 12.987;
          i = TrNumber(myD, out myDr, out mySqr, out mySqrt);
-
          Console.WriteLine("Исходное число: {0}\nЦелая часть числа: {1}\nДробная часть числа: {2}\nКвадрат числа: {3}\nКвадратный корень числа: {4}",
-            myD,
-            i,
-            myDr,
-            mySqr,
-            mySqrt);
+            myD, i, myDr, mySqr, mySqrt);
+
+         int x = 1, y = 2, a = 3, b = 4;
+         Console.WriteLine("До вызова: \tx={0}; y ={1}; a ={2}; b ={3}", x, y, a, b);
+         Add(x, y, out a, out b);
+         Console.WriteLine("После вызова: \tx={0}; y ={1}; a ={2}; b ={3}", x, y, a, b);
+
 
          Console.ReadKey();
+      }
+
+      // Выходные параметры, представленные значением
+      public static int Add(int x, int y, out int a, out int b)
+      {
+         int answer = x + y;
+         x = 10; y = 20; a = 30; b = 40;
+         return answer;
       }
 
       // Метод возвращающий целую и дробную части числа, квадрат и корень числа
@@ -30,7 +39,6 @@ namespace CodeExperiment
          dr = d - i;
          sqr = d * d;
          sqrt = Math.Sqrt(d);
-
          return i;
       }
    }
